016e43b88a425e70b46ade2bd4cabf7e2bfac39a
PratikDokhe <79921446+PratikDokhe@users.noreply.github.com>
2025-04-20 08:43:11 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,20 @@
+/*
+
+======================================================================================================
+Create Database and Schemas
+======================================================================================================
+
+*/
+
+
+
+--Create Database DataWarehouse;
+
+
+--Use DataWarehouse;
+
+--Create schema bronze;
+--Create schema silver;
+--Create schema gold;
+
+
