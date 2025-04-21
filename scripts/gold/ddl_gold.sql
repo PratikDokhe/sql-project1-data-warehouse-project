9065799c18c3d356e72833ab79668fca906bb040
PratikDokhe <79921446+PratikDokhe@users.noreply.github.com>
2025-04-21 19:01:13 +0530

    Create ddl_gold.sql

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,105 @@
+/*
+
+=======================================================================
+DDL Script: Create Gold Tables
+=======================================================================
+
+*/
+
+-- Note you have to Run Each gold table SQL seprately.
+
+
+
+create view gold.dim_customers as
+
+select 
+row_number() over(order by cust_id) as customer_key,
+ci.cust_id as customer_id,
+ci.cust_key as customer_number,
+ci.cst_firstname as first_name,
+ci.cst_lastname as last_name,
+la.CNTRY as country,
+ci.cst_marital_status as marital_status,
+case 
+	when ci.cst_gndr = 'n/a' then coalesce(ca.GEN,'n/a')
+	else ci.cst_gndr
+end as gender, --- CRM is master for gender
+--ci.cst_gndr,
+ca.BDATE as birthday,
+ci.cst_create_date as create_date
+
+--ca.GEN,
+
+
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca on ci.cust_key=ca.cid
+left join silver.erp_loc_a101 la on ci.cust_key=la.CID;
+
+
+-- ##################################################################################
+
+create view gold.dim_products as
+
+with crm_prd_info as(
+
+select 
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt
+from(
+select 
+pn.prd_id,
+pn.cat_id,
+pn.prd_key,
+pn.prd_nm,
+pn.prd_cost,
+pn.prd_line,
+pn.prd_start_dt,
+--pn.prd_end_dt,
+rank() over(partition by prd_key order by prd_id desc) as r 
+from silver.crm_prd_info pn
+)t where r=1 -- Filtering out all hostorical data
+
+)
+
+select 
+row_number() over(order by pn.prd_start_dt,pn.prd_key) as product_key,
+pn.prd_id as product_id,
+pn.prd_key as product_number,
+pn.prd_nm as product_name,
+pn.cat_id as category_id,
+pc.cat as category,
+pc.SUBCAT as subcategory,
+pc.MAINTENANCE as maintenance,
+pn.prd_cost as cost,
+pn.prd_line as product_line,
+pn.prd_start_dt as start_date
+
+
+from crm_prd_info pn 
+left join silver.erp_px_cat_g1v2 pc on pn.cat_id=pc.id;
+
+-- ##############################################################################################
+
+create view gold.fact_sales as
+
+select 
+sd.sls_ord_num as order_number,
+pr.product_key,
+--sd.sls_prd_key,
+cu.customer_key,
+--sd.sls_cust_id,
+sd.sls_order_dt as order_date,
+sd.sls_ship_dt as shipping_date,
+sd.sls_due_dt as due_date,
+sd.sls_sales as sales_amount,
+sd.sls_quantity as quantity,
+sd.sls_price as price
+
+from silver.crm_sales_details sd
+left join gold.dim_products pr on sd.sls_prd_key=pr.product_number
+left join gold.dim_customers cu on sd.sls_cust_id=cu.customer_id;
